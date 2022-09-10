64ef2bf81109433a8411771444aa8ff12519ddd4
Milles Jethro D Del Rosario <milles.jethro16@gmail.com>
2022-09-10 09:16:11 +0800

    binding

--- app/src/main/java/com/auf/activity/MainScreen.kt
+++ app/src/main/java/com/auf/activity/MainScreen.kt
@@ -4,17 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import com.auf.activity.databinding.ActivityMainScreenBinding
+import com.auf.activity.databinding.ActivityRegisterBinding
 
 class MainScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityMainScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_screen)
+        binding = ActivityMainScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val regis = findViewById<Button>(R.id.reg)
 
-        regis.setOnClickListener {
+
+        binding.reg.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
